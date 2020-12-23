0810ea3482a9f6ed30337d0d18a05ca41ebc226f
Joe Masilotti <joe@masilotti.com>
2020-12-23 11:26:06 -0500

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "Turbo-Auth",
+    platforms: [
+        .iOS(.v13),
+    ],
+    dependencies: [],
+    targets: []
+)
